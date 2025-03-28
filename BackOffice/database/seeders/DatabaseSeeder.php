495939a0573bbe900aa9d4216ade17b4e3fb0005
feffe1702 <fpm.asd@gmail.com>
2025-03-28 14:05:06 +0100

    Add user seeder to create default users with roles and attributes

--- BackOffice/database/seeders/DatabaseSeeder.php
+++ BackOffice/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +15,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         $this->call(Roles::class);
+        $this->createUsers();
+    }
+
+    private function createUsers()
+    {
+        // Ottieni i ruoli
+        $guestRole = Role::where('name', 'guest')->first();
+        $operatorRole = Role::where('name', 'operator')->first();
+        $adminRole = Role::where('name', 'admin')->first();
+
+        // Creazione utenti con i rispettivi ruoli
+        User::create([
+            'name' => 'Guest',
+            'surname' => 'Guest',
+            'phone' => '1234567890',
+            'email' => 'guest@example.com',
+            'password' => Hash::make('Password123!'),
+            'role_id' => $guestRole->id,
+            'is_available' => false,
+        ]);
+
+        User::create([
+            'name' => 'Operator',
+            'surname' => 'Operator',
+            'phone' => '0987654321',
+            'email' => 'operator@example.com',
+            'password' => Hash::make('Password123!'),
+            'role_id' => $operatorRole->id,
+            'is_available' => true,
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'surname' => 'Admin',
+            'phone' => '1122334455',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('Password123!'),
+            'role_id' => $adminRole->id,
+            'is_available' => true,
+        ]);
     }
 }
